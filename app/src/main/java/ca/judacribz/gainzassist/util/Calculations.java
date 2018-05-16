8caefe340f56922039588074b3483c81ed36c3c0
judacribz <sheron.balas@gmail.com>
2018-05-15 22:34:15 -0400

    fixed up exercise button width

--- app/src/main/java/ca/judacribz/gainzassist/util/Calculations.java
+++ app/src/main/java/ca/judacribz/gainzassist/util/Calculations.java
@@ -5,10 +5,19 @@ import android.util.DisplayMetrics;
 
 public class Calculations {
 
+    // Constants
+    // --------------------------------------------------------------------------------------------
+    private final static float GRID_ITEM_WIDTH = 120;
+    // --------------------------------------------------------------------------------------------
+
+    // Global Vars
+    // --------------------------------------------------------------------------------------------
+    // --------------------------------------------------------------------------------------------
+    /* Gets the optimum number of columns for a GridLayout based on the screen width and density */
     public static int getNumColumns(Context context) {
         DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
         float width = displayMetrics.widthPixels / displayMetrics.density;
 
-        return (int) (width / 180);
+        return (int) (width / GRID_ITEM_WIDTH);
     }
 }
