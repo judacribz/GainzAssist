004e64160ff1036d4a1043518d72eeb6247d4280
judacribz <sheron.balas@gmail.com>
2018-09-03 22:21:56 -0400

    more workout functionality

--- app/src/main/java/ca/judacribz/gainzassist/Main.java
+++ app/src/main/java/ca/judacribz/gainzassist/Main.java
@@ -7,6 +7,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
 import butterknife.*;
 
 import ca.judacribz.gainzassist.activities.authentication.Login;
--- app/src/main/java/ca/judacribz/gainzassist/activities/start_workout/fragments/CurrWorkout.java
+++ app/src/main/java/ca/judacribz/gainzassist/activities/start_workout/fragments/CurrWorkout.java
@@ -5,13 +5,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Locale;
@@ -24,13 +28,19 @@ import ca.judacribz.gainzassist.activities.how_to_videos.HowToVideos;
 import ca.judacribz.gainzassist.activities.start_workout.EquipmentView;
 import ca.judacribz.gainzassist.activities.start_workout.StartWorkout;
 import ca.judacribz.gainzassist.models.Exercise;
+import ca.judacribz.gainzassist.models.Set;
 import ca.judacribz.gainzassist.models.User;
 
-public class CurrWorkout extends Fragment{
+public class CurrWorkout extends Fragment {
 
     // Constants
     // --------------------------------------------------------------------------------------------
     public static final String EXTRA_HOW_TO_VID = "ca.judacribz.gainzassist.EXTRA_HOW_TO_VID";
+    public static final float BB_MIN_WEIGHT = 45.0f;
+    public static final float MIN_WEIGHT = 0.0f;
+    public static final float BB_WEIGHT_CHANGE = 5.0f;
+    public static final float WEIGHT_CHANGE = 2.5f;
+    public static final int MIN_REPS = 0;
     // --------------------------------------------------------------------------------------------
 
     // Global Vars
@@ -39,12 +49,28 @@ public class CurrWorkout extends Fragment{
     EquipmentView equipmentView;
     CountDownTimer countDownTimer;
     Exercise currExercise;
+    ArrayList<Set> currSets;
+    Set currSet;
+    String currExerciseName, currEquipment;
+    ArrayList<Exercise> warmups;
+
+    float currWeight, weight, minWeight = MIN_WEIGHT, weightChange = WEIGHT_CHANGE;
+    int currReps, reps;
+
+
 
     // --------------------------------------------------------------------------------------------
     // UI Elements
     @BindView(R.id.rl_equip_disp) ViewGroup vgEquipDisp;
-    @BindView(R.id.tv_timer) TextView tv_timer;
+    @BindView(R.id.tv_timer) TextView tvTimer;
+    @BindView(R.id.tv_exercise_title) TextView tvExerciseTitle;
+    @BindView(R.id.tv_set_info) TextView tvSetInfo;
+    @BindView(R.id.tv_ex_info) TextView tvExInfo;
     @BindView(R.id.btn_how_to) Button btnHowTo;
+    @BindView(R.id.btn_dec_reps) ImageButton btnDecReps;
+    @BindView(R.id.btn_dec_weight) ImageButton btnDecWeight;
+    @BindView(R.id.et_curr_reps) EditText etCurrReps;
+    @BindView(R.id.et_curr_weight) EditText etCurrWeight;
     // --------------------------------------------------------------------------------------------
 
     // ######################################################################################### //
@@ -65,6 +91,22 @@ public class CurrWorkout extends Fragment{
     public void onAttach(Context context) {
         super.onAttach(context);
         act = (StartWorkout) getActivity();
+
+        warmups = User.getInstance().getWarmups();
+
+        currExercise = warmups.get(0);
+        currExerciseName = currExercise.getName();
+        currSets = currExercise.getSets();
+        currSet = currSets.get(0);
+        weight = currWeight = currSet.getWeight();
+        reps = currReps = currSet.getReps();
+
+        currEquipment = currExercise.getEquipment();
+
+        if (currEquipment.toLowerCase().equals(getString(R.string.barbell))) {
+            minWeight = BB_MIN_WEIGHT;
+            weightChange = BB_WEIGHT_CHANGE;
+        }
     }
 
     @Override
@@ -73,50 +115,110 @@ public class CurrWorkout extends Fragment{
     }
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_curr_workout, container, false);
         ButterKnife.bind(this, view);
+
+        String info = "Warmup 1/" + warmups.size() + "\nSet 1/" + currExercise.getNumSets();
+        tvExInfo.setText(String.format("Warmup 1/%s", warmups.size()));
+        tvSetInfo.setText(String.format("Set 1/%s", currExercise.getNumSets()));
+        tvExerciseTitle.setText(currExerciseName);
+
         setupEquipView();
-        setupTimer();
+        setRepsWeight();
+        startTimer(500000);
 
         return view;
     }
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
+    private void setRepsWeight() {
+        setReps();
+        setWeight();
 
-        ArrayList<Exercise> warmups = User.getInstance().getWarmups();
-
-        currExercise = warmups.get(0);
-        btnHowTo.setText(String.format("How To %s", currExercise.getName()));
-    }
-
-    public void setupEquipView() {
-        // Set up the custom view (EquipmentView) to display the equipment. View added dynamically
-        // to trigger onDraw method
-        equipmentView = new EquipmentView(act);
-        equipmentView.setLayoutParams(
-                new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
-                        ViewGroup.LayoutParams.MATCH_PARENT)
-        );
-        vgEquipDisp.addView(equipmentView, 0);
-
-        //TODO: make deterministic
-        equipmentView.post(new Runnable() {
+        etCurrReps.addTextChangedListener(new TextWatcher() {
             @Override
-            public void run() {
-                equipmentView.setup(315, getString(R.string.barbell));
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                if (!btnDecReps.isEnabled())
+                    btnDecReps.setEnabled(true);
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (!s.toString().isEmpty()) {
+                    reps = Integer.valueOf(s.toString());
+                } else {
+                    reps = MIN_REPS;
+                }
+
+                if (reps == MIN_REPS) {
+                    btnDecReps.setEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+
+        etCurrWeight.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                if (!btnDecWeight.isEnabled()) {
+                    btnDecWeight.setEnabled(true);
+                }
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (!s.toString().isEmpty()) {
+                    weight = Float.valueOf(s.toString());
+                    equipmentView.setup(weight, getString(R.string.barbell));
+                } else {
+                    weight = minWeight;
+                }
+
+                if ((weight - weightChange) < minWeight) {
+                    btnDecWeight.setEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
             }
         });
     }
 
-    public void setupTimer() {
-        countDownTimer = getCountDownTimer(5000);
+    // Set up the custom view (EquipmentView) to display the equipment. View added dynamically
+    // to trigger onDraw method
+    public void setupEquipView() {
+        equipmentView = new EquipmentView(act);
+        equipmentView.setLayoutParams(new ViewGroup.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT
+        ));
+
+        vgEquipDisp.addView(equipmentView, 0);
+
+        setEquipViewWeight();
+    }
+
+    public void setEquipViewWeight() {
+        equipmentView.post(new Runnable() {
+            @Override
+            public void run() {
+                equipmentView.setup(weight, getString(R.string.barbell));
+            }
+        });
+    }
+
+    public void startTimer(long timeInMillis) {
+        countDownTimer = getCountDownTimer(timeInMillis);
         countDownTimer.start();
     }
+
     /* Creates and returns a new CountDownTimer with the rest time to count down from. Has the
      * following format: 0:00
      */
@@ -124,7 +226,6 @@ public class CurrWorkout extends Fragment{
     CountDownTimer getCountDownTimer(long milliseconds) {
 
         return new CountDownTimer(milliseconds, 1000) {
-
             String timerText = "";
             long seconds;
             long minutes;
@@ -137,7 +238,7 @@ public class CurrWorkout extends Fragment{
                 seconds = seconds % 60;
 
                 timerText = minutes + ":" + String.format(Locale.getDefault(), "%02d", seconds);
-                tv_timer.setText(timerText);
+                tvTimer.setText(timerText);
             }
 
             // Changes the timer text, when it gets to 0:00, to "Start the next set"
@@ -145,6 +246,13 @@ public class CurrWorkout extends Fragment{
             }
         };
     }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        countDownTimer.cancel();
+    }
+
     //Fragment//Override///////////////////////////////////////////////////////////////////////////
 
 
@@ -153,8 +261,53 @@ public class CurrWorkout extends Fragment{
     @OnClick(R.id.btn_how_to)
     public void startWorkoutsList() {
         Intent intent = new Intent(act, HowToVideos.class);
-        intent.putExtra(EXTRA_HOW_TO_VID, currExercise.getName());
+        intent.putExtra(EXTRA_HOW_TO_VID, currExerciseName);
         startActivity(intent);
+    }
+
+    @OnClick(R.id.tv_timer)
+    public void changeTimerState() {
+        if (currTime/1000 != 0) {
+            if (countDownTimer == null) {
+                startTimer(currTime);
+            } else {
+                countDownTimer.cancel();
+                countDownTimer = null;
+            }
+        }
+    }
+
+    @OnClick(R.id.btn_dec_reps)
+    public void decReps() {
+        reps--;
+        setReps();
+    }
+
+    @OnClick(R.id.btn_inc_reps)
+    public void incReps() {
+        reps++;
+        setReps();
+    }
+
+    public void setReps() {
+        etCurrReps.setText(String.valueOf(reps));
+    }
+
+    @OnClick(R.id.btn_dec_weight)
+    public void decWeight() {
+        weight -= weightChange;
+        setWeight();
+    }
+
+    @OnClick(R.id.btn_inc_weight)
+    public void incWeight() {
+        weight += weightChange;
+        setWeight();
+    }
+
+    public void setWeight() {
+        etCurrWeight.setText(String.valueOf(Math.max(weight, minWeight)));
+        equipmentView.setup(weight, getString(R.string.barbell));
     }
     //=Click=Handling==============================================================================
 }
